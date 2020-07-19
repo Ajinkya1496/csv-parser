beb674fb763ad7ab9f8689ccea3048bd8af0dbf8
salil0001 <salilsharma.sharma@gmail.com>
2020-07-19 15:21:36 +0530

    Added ignoreErrors API and changed csv file.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const parser =  require('./parser/parser');
 
-let json = parser('./sample.csv', {ignoreHeader: true});
-
+let json = parser('./sample.csv', {ignoreHeader: true , ignoreErrors:true});
 json.then(data => {
     console.log("Result\n", data);
 })
--- parser/parser.js
+++ parser/parser.js
@@ -1,68 +1,68 @@
-const fs = require('fs');
+const fs = require("fs");
 
-function parser(filepath, { ignoreHeader = false }) {
-    if (typeof filepath !== 'string') {
-        throw new TypeError(`Expected string got ${typeof filepath}`);
-    }
-    return new Promise((resolve) => {
-        let fileData = '';
-        // try {
-        //     fileData = fs.readFileSync(filepath, {encoding: 'utf-8'});
-        // } catch(error) {
-        //     throw new Error(`File not found ${filepath}`);
-        // }
-        let readStream = fs.createReadStream(filepath, { encoding: 'utf-8' });
-        readStream.on('data', chunk => {
-            fileData += chunk;
-        });
+function parser(filepath, { ignoreHeader = false, ignoreErrors = true }) {
+  console.log(ignoreErrors);
+  if (typeof filepath !== "string") {
+    throw new TypeError(`Expected string got ${typeof filepath}`);
+  }
+  return new Promise((resolve) => {
+    let fileData = "";
+    let readStream = fs.createReadStream(filepath, { encoding: "utf-8" });
+    readStream.on("data", (chunk) => {
+      fileData += chunk;
+    });
 
-        readStream.on('end', () => {
-            let delimiter = detectDelimiter(fileData)[0];
-            let allLines = fileData.split(/\r\n|\n/);
+    readStream.on("end", () => {
+      let delimiter = detectDelimiter(fileData)[0];
+      let allLines = fileData.split(/\r\n|\n/);
+      let headersFields = allLines[0].split(delimiter);
 
-            let headersFields = allLines[0].split(delimiter);
-            let parsedData = [];
+      const headersFieldsLength = headersFields[0].split(";").length;
 
-            let i = ignoreHeader === true ? 0 : 1;
-
-            for (; i < allLines.length; i++) {
-                const data = allLines[i].split(delimiter);
-                if (ignoreHeader === true) {
-                    parsedData.push(data);
-                } else {
-                    if (data.length === headersFields.length) {
-                        let obj = {};
-                        for (var j = 0; j < headersFields.length; j++) {
-                            obj[headersFields[j]] = data[j];
-                        }
-                        parsedData.push(obj);
-                    }
-                }
+      let parsedData = [];
+      let i = ignoreHeader === true ? 0 : 1;
+      for (; i < allLines.length; i++) {
+        const data = allLines[i].split(delimiter);
+        let dataLength = data[0].split(";").length;
+        if (headersFieldsLength !== dataLength) {
+          if (ignoreErrors) continue;
+          else parsedData.push(["error"]);
+        } else if (ignoreHeader === true) {
+          parsedData.push(data);
+        } else {
+          if (data.length === headersFields.length) {
+            let obj = {};
+            for (var j = 0; j < headersFields.length; j++) {
+              obj[headersFields[j]] = data[j];
             }
-            resolve(parsedData);
-        })
-    })
+            parsedData.push(obj);
+          }
+        }
+      }
+      resolve(parsedData);
+    });
+  });
 }
 
 function detectDelimiter(text) {
-    let possibleDelimiters = [',', ';', '\t'];
-    return possibleDelimiters.filter(filterOut);
+  let possibleDelimiters = [",", ";", "\t"];
+  return possibleDelimiters.filter(filterOut);
 
-    function filterOut(delimiter) {
-        let cache = -1;
-        return text.split('\n').every(checkLength);
+  function filterOut(delimiter) {
+    let cache = -1;
+    return text.split("\n").every(checkLength);
 
-        function checkLength(line) {
-            if (!line) {
-                return true;
-            }
-            let length = line.split(delimiter).length;
-            if (cache < 0) {
-                cache = length;
-            }
-            return cache === length && length > 1;
-        }
+    function checkLength(line) {
+      if (!line) {
+        return true;
+      }
+      let length = line.split(delimiter).length;
+      if (cache < 0) {
+        cache = length;
+      }
+      return cache === length && length > 1;
     }
+  }
 }
 
 module.exports = parser;
