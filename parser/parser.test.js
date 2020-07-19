11fdcf03f6ca51bbd065ffefba6dadad9c574e10
salil0001 <salilsharma.sharma@gmail.com>
2020-07-19 23:05:36 +0530

    2 test cases and 2 sub test cases added.

--- parser/parser.test.js
+++ parser/parser.test.js
@@ -1,27 +1,62 @@
-const parser = require('./parser');
+const parser = require("./parser");
 
-describe('API ignore headers.', () => {
+describe("API ignore headers.", () => {
+  test("CSV data for ignoreHeader: true.", async () => {
+    return parser("./sample.csv", { ignoreHeader: true }).then((data) => {
+      expect(data).toEqual([
+        ["id", " name", " marks"],
+        ["1", "Ajinkya", "20"],
+        ["2", "Hitman", "30"],
+        ["3", "Arfat", "40"],
+        ["4", "Aditya", "50"],
+      ]);
+    });
+  });
 
-    test('CSV data for ignoreHeader: true', async () => {
-        return parser('./sample.csv', {ignoreHeader: true}).then(data => {
-          expect(data).toEqual([
-            [ 'id', ' name', ' marks' ],
-            [ '1', 'Ajinkya', '20' ],
-            [ '2', 'Hitman', '30' ],
-            [ '3', 'Arfat', '40' ],
-            [ '4', 'Aditya', '50' ]
-          ]);
-        });
-      });
+  test("CSV data for ignoreHeader: false.", async () => {
+    return parser("./sample.csv", { ignoreHeader: false }).then((data) => {
+      expect(data).toEqual([
+        { id: "1", " name": "Ajinkya", " marks": "20" },
+        { id: "2", " name": "Hitman", " marks": "30" },
+        { id: "3", " name": "Arfat", " marks": "40" },
+        { id: "4", " name": "Aditya", " marks": "50" },
+      ]);
+    });
+  });
+});
 
-    test('CSV data for ignoreHeader: false', async () => {
-        return parser('./sample.csv', {ignoreHeader: false}).then(data => {
-          expect(data).toEqual([
-            { id: '1', ' name': 'Ajinkya', ' marks': '20' },
-            { id: '2', ' name': 'Hitman', ' marks': '30' },
-            { id: '3', ' name': 'Arfat', ' marks': '40' },
-            { id: '4', ' name': 'Aditya', ' marks': '50' }
-          ]);
-        });
-      });
-})
+describe("API for callbacks", () => {
+  test("CSV data for callback function.", (done) => {
+    function callback(data) {
+      try {
+        expect(data).toEqual([
+          { id: "1", " name": "Ajinkya", " marks": "20" },
+          { id: "2", " name": "Hitman", " marks": "30" },
+          { id: "3", " name": "Arfat", " marks": "40" },
+          { id: "4", " name": "Aditya", " marks": "50" },
+        ]);
+        done();
+      } catch (error) {
+        done(error);
+      }
+    }
+    parser("./sample.csv", { ignoreHeader: false }, callback);
+  });
+  test("CSV data for callback function with ignoreHeader.", (done) => {
+    function callback(data) {
+      try {
+        expect(data).toEqual([
+          [ 'id', ' name', ' marks' ],
+          [ '1', 'Ajinkya', '20' ],
+          [ '2', 'Hitman', '30' ],
+          [ '3', 'Arfat', '40' ],
+          [ '4', 'Aditya', '50' ]
+        ]);
+        done();
+      } catch (error) {
+        done(error);
+      }
+    }
+    parser("./sample.csv", { ignoreHeader: true }, callback);
+  });
+});
